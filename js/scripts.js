08f0b9b100ab2e368b4ab8b858c7fd93a2ddc0c8
Nimisoere Tekena-Lawson <lawson.nimi@yahoo.com>
2017-01-10 23:54:49 +0100

    New files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿$('.form-control').on('focus blur', function (e) {
+    $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+}).trigger('blur');
+
+$(document).ready(function () {
+
+    $('.remove-allel:first').hide();
+    
+    $('.add-dna').click(function () {
+        //we select the box clone it and insert it after the box
+        $('.box:first').clone().insertAfter(".box:last");
+        $(".remove-allel:not('.remove-allel:first')").show();
+    });
+
+    $(document).on("click", ".remove-allel", function () {
+        $(this).closest(".box").remove();
+    });
+});
